982981807ff473073e719e0e26bfc49a872591c5
Mohammad Anwari <mdamt@mdamt.net>
2014-09-19 11:09:57 +0700

    Basic angular setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require("gulp");
 var concat  = require("gulp-concat");
 var shell = require("gulp-shell");
+var files = require("./files");
 
 var webodfVersion = "0.5.4";
 
@@ -9,6 +10,23 @@ gulp.task("webodf", shell.task([
   "cd ./vendor/webodf;wget -c http://webodf.org/download/webodf.js-VERSION.zip;unzip webodf.js-VERSION.zip".replace(/VERSION/g, webodfVersion),
   "mv ./vendor/webodf/webodf.js-VERSION/webodf.js ./vendor/webodf/".replace(/VERSION/g, webodfVersion)
 ]));
+
+gulp.task("src", function() {
+  gulp.src(files.src)
+  .pipe(concat("angular-webodf.js"))
+  .pipe(gulp.dest("./dist/"))
+});
+
+gulp.task("demo-libs", function() {
+  gulp.src(files["demo-libs"])
+  .pipe(concat("libs.js"))
+  .pipe(gulp.dest("./demo/"))
+});
+
+gulp.task("demo", ["demo-libs", "src"], function() {
+  gulp.src("./dist/angular-webodf.js")
+  .pipe(gulp.dest("./demo/"))
+});
 
 gulp.task("default", [
     ]);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+var ngWebODF = angular.module("ngWebODF", [])
